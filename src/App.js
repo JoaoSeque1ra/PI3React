2288a2cb46cf3b058033f23c49961e8a0b157957
João Sequeira <joaosequeira28@hotmail.com>
2022-07-01 15:49:13 +0100

    beggin routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './Styles/style.css';
@@ -7,8 +9,15 @@ import Dashboard from './Pages/Dashboard';
 
 export default function App() {
   return (
-    <>
-      <Client.Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="client/" element={<Client.Home />}/>
+        <Route path="client/marketing-digital" element={<Client.MarketingDigital />}/>
+        <Route path="client/design-grafico" element={<Client.DesignGrafico />}/>
+        <Route path="client/comunicacao-consultoria" element={<Client.ComunicacaoConsultoria />}/>
+
+        <Route path="dashboard/" element={<Dashboard.Home />}/>
+      </Routes>
+    </Router>
   );
 }
