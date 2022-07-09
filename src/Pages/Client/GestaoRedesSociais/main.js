59d5e2906fc5659030e2aa1288fa262426d52cc7
Raquelmeida <raquelalmeida-15@outlook.pt>
2022-07-09 10:13:51 +0100

    criacao da pagina gestao redes sociais

---
+++ src/Pages/Client/GestaoRedesSociais/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Breadcrumbs from '../../../Components/Breadcrumb';
+import CheckBox from '../../../Components/CheckBox';
+import CardsCheckBox from '../../../Components/CardsCheckBox';
+import Buttons from '../../../Components/Buttons';
+
+export default function Main() {
+    return (
+        
+        
+    );
+}
