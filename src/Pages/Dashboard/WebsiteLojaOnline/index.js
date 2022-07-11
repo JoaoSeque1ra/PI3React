7b6cdd091d7c662ea04aa38f7783cb9bdb372c14
João Sequeira <joaosequeira28@hotmail.com>
2022-07-11 15:50:11 +0100

    website e loja online

--- src/Pages/Dashboard/WebsiteLojaOnline/index.js
+++ src/Pages/Dashboard/WebsiteLojaOnline/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import NavbarDashboardMd from '../../../Components/Dashboard/NavbarDashboard/NavbarDashboardMd';
+import Main from './main';
 
 export function WebsiteLojaOnline() {
     return (  
         <>
             <NavbarDashboardMd />
+            <Main />
         </>
     );
 }
