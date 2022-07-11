583022418a30517b4a4819a8050df048210469c4
Raquelmeida <raquelalmeida-15@outlook.pt>
2022-07-11 12:36:22 +0100

    atualizacao packs

--- src/Pages/Client/GestaoRedesSociais/main.js
+++ src/Pages/Client/GestaoRedesSociais/main.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import Breadcrumbs from '../../../Components/Breadcrumb';
-import CheckBox from '../../../Components/CheckBox';
-import CardsCheckBox from '../../../Components/CardsCheckBox';
+
 import Buttons from '../../../Components/Buttons';
 
 export default function Main() {
@@ -10,37 +9,39 @@ export default function Main() {
         <main>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-12 col-lg-7 pt-pageLeft">
-                        <div className="row">
-                        <Breadcrumbs route="/ Marketing Digital / Gestão de Redes Sociais" textColor="text-yellow"/>
 
-                        <div className="col-md-12 offset-lg-1 col-lg-2">
-                            <div className='mt-5'>
-                                <div className="card card-paidmedia-gestaoredessociais">
+                    <div className="col-md-12 offset-lg-1 col-lg-10 font-weight-semiBold font-size-1__5rem pt-pageLeft">
+                        <Breadcrumbs route="/ Marketing Digital / Gestão de Redes Sociais" textColor="text-yellow"/>
+                    </div>
+
+                    <div className="col-12 offset-lg-1 col-lg-10">
+                        <div className="row">
+
+                            <div className="col-md-6 col-lg-3 mb-5 mt-2">
+                                <div className="card text-center h-100 border-warning mt-5" style={{ borderRadius: "3rem", maxHeight:"38rem" }}>
                                     <div className="card-body">
-                                        <h6 className="card-subtitle mt-3 text-muted">Plano Mensal</h6>
-                                        <p className="card-text mb-5 text-yellow fw-bold">START</p>
-                                        <p className="card-text">2 redes sociais</p>
-                                        <p className="card-text">2 posts/semana</p>
-                                        <p className="card-text">2 stories/semana</p>
-                                        <p className="card-text">1 cover | capa</p>
-                                        <p className="card-text">Design gráfico</p>
-                                        <p className="card-text">Copywriting</p>
-                                        <p className="card-text"> X Planificação editorial</p>
-                                        <p className="card-text"> X Consultoria Digital</p>
-                                        <p className="card-text"> X Gestão de campanhas</p>
-                                        <p className="card-text"> X Relatório estatístico</p>
-                                    </div>
+                                    <h6 class="card-subtitle mt-3 text-muted">Plano Mensal</h6>
+                                    <p className="card-text mb-5 text-yellow fw-bold">START</p>
+                                    <p className="card-text">2 redes sociais</p>
+                                    <p className="card-text">2 posts/semana</p>
+                                    <p className="card-text">2 stories/semana</p>
+                                    <p className="card-text">1 cover | capa</p>
+                                    <p className="card-text">Design gráfico</p>
+                                    <p className="card-text">Copywriting</p>
+                                    <p className="card-text"> X Planificação editorial</p>
+                                    <p className="card-text"> X Consultoria Digital</p>
+                                    <p className="card-text"> X Gestão de campanhas</p>
+                                    <p className="card-text"> X Relatório estatístico</p>
+                                </div>
+                            
+                                </div>
+                                <div className="text-center mt-3 mb-2">
+                                    <Buttons color="btn-yellow" text="Selecionar"/>
                                 </div>
                             </div>
-                            <div className="col-md-12 col-lg-2 text-center mt-3 mb-5 mt-5">
-                                <Buttons color="btn-yellow" text="Selecionar"/>
-                            </div>
-                        </div>
-
-                        <div className="col-md-12 col-lg-2">
-                            <div className="mt-5">
-                                <div className="card card-paidmedia-gestaoredessociais">
+                            
+                            <div className="col-md-6 col-lg-3 mb-5 mt-2">
+                                <div className="card text-center h-100 border-warning mt-5" style={{ borderRadius: "3rem", maxHeight:"38rem" }}>
                                     <div className="card-body">
                                         <h6 className="card-subtitle mt-3 text-muted">Plano Mensal</h6>
                                         <p className="card-text mb-5 text-yellow fw-bold">BOOST</p>
@@ -56,15 +57,13 @@ export default function Main() {
                                         <p className="card-text mb-3"> X Relatório estatístico</p>
                                     </div>
                                 </div>
-                                <div className="col-md-12 col-lg-2 text-center mt-3 mb-5 mt-5">
+                                <div className="text-center mt-3 mb-2">
                                     <Buttons color="btn-yellow" text="Selecionar"/>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className="col-md-12 col-lg-2">
-                            <div className="mt-5">
-                                <div className="card card-paidmedia-gestaoredessociais">
+                            <div className="col-md-6 col-lg-3 mb-5 mt-2">
+                                <div className="card text-center h-100 border-warning mt-5" style={{ borderRadius: "3rem", maxHeight:"38rem" }}>
                                     <div className="card-body">
                                         <h6 className="card-subtitle mt-3 text-muted">Plano Mensal</h6>
                                         <p className="card-text mb-5 text-yellow fw-bold">BOMB</p>
@@ -79,18 +78,18 @@ export default function Main() {
                                         <p className="card-text mb-3"> - Gestão de campanhas</p>
                                         <p className="card-text mb-3"> - Relatório estatístico</p>
                                     </div>
+                            
+                            
                                 </div>
-                                <div className="col-md-12 col-lg-2 text-center mt-3 mb-5 mt-5">
+                                <div className="text-center mt-3 mb-2">
                                     <Buttons color="btn-yellow" text="Selecionar"/>
                                 </div>
                             </div>
-                        </div>
-
-                        <div className="col-md-12 col-lg-2">
-                            <div className="mt-5">
-                                <div className="card card-paidmedia-gestaoredessociais bg-yellow">
+                            
+                            <div className="col-md-6 col-lg-3 mb-5 mt-2">
+                                <div className="card text-center h-100 border-warning bg-yellow mt-5" style={{ borderRadius: "3rem", maxHeight:"38rem" }}>
                                     <div className="card-body">
-                                        <h6 className="card-subtitle mt-3 text-muted">Plano Mensal/Anual</h6>
+                                        <h6 clasNames="card-subtitle mt-3 text-muted">Plano Mensal/Anual</h6>
                                         <p className="card-text mb-5 text-white fw-bold">PERSONALIZADO</p>
                                         <p className="card-text mb-3">nº redes sociais</p>
                                         <p className="card-text mb-3">nº posts/semana</p>
@@ -103,12 +102,13 @@ export default function Main() {
                                         <p className="card-text mb-3"> - Gestão de campanhas</p>
                                         <p className="card-text mb-3"> - Relatório estatístico</p>
                                     </div>
+                        
                                 </div>
-                                <div className="col-md-12 col-lg-2 text-center mb-5 mt-5">
+                                <div className="text-center mt-3 mb-2">
                                     <Buttons color="btn-yellow" text="Selecionar"/>
                                 </div>
                             </div>
-                        </div>
+
                         </div>
                     </div>
                 </div>
--- src/Pages/Client/PaidMedia/main.js
+++ src/Pages/Client/PaidMedia/main.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import Breadcrumbs from '../../../Components/Breadcrumb';
-import CheckBox from '../../../Components/CheckBox';
-import CardsCheckBox from '../../../Components/CardsCheckBox';
 import Buttons from '../../../Components/Buttons';
 
 export default function Main() {
@@ -10,15 +8,18 @@ export default function Main() {
         <main>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-12 col-lg-7 pt-pageLeft">
-                        <div className="row">
-                        <Breadcrumbs route="/ Marketing Digital / Paid Media" textColor="text-yellow"/>
 
-                        <div className="col-md-12 offset-lg-1 col-lg-2">
-                            <div className='mt-5'>
-                                <div className="card card-paidmedia-gestaoredessociais">
+                    <div className="col-md-12 offset-lg-1 col-lg-10 font-weight-semiBold font-size-1__5rem pt-pageLeft">
+                        <Breadcrumbs route="/ Marketing Digital / Paid Media" textColor="text-yellow"/>
+                    </div>
+
+                    <div className="col-12 offset-lg-1 col-lg-10">
+                        <div className="row">
+
+                            <div className="col-md-6 col-lg-3 mb-5 mt-2">
+                                <div className="card text-center h-100 border-warning mt-5" style={{ borderRadius: "3rem", maxHeight:"44rem" }}>
                                     <div className="card-body">
-                                        <h5 className="card-title mb-5 mt-2 fw-bolder">Facebook/Instagram Ads</h5>
+                                        <h5 className="card-title mb-5 mt-2 fw-bolder">Facebook/ Instagram Ads</h5>
                                         <h6 className="card-subtitle text-muted">Plano Mensal</h6>
                                         <p className="card-text mb-3 text-yellow fw-bold">BASIC</p>
                                         <p className="card-text mb-3">Estratégia Digital</p>
@@ -27,16 +28,15 @@ export default function Main() {
                                         <p className="card-text mb-3">Otimização da Campanha</p>
                                         <p className="card-text mb-3">Relatório mensal</p>
                                     </div>
+                            
+                                </div>
+                                <div className="text-center mt-3 mb-2">
+                                    <Buttons color="btn-yellow" text="Selecionar"/>
                                 </div>
                             </div>
-                            <div className="col-md-12 col-lg-2 text-center mt-3 mb-5 mt-5">
-                                <Buttons color="btn-yellow" text="Saber Valores"/>
-                            </div>
-                        </div>
-
-                        <div className="col-md-12 col-lg-2">
-                            <div className="mt-5">
-                                <div className="card card-paidmedia-gestaoredessociais">
+                            
+                            <div className="col-md-6 col-lg-3 mb-5 mt-2">
+                                <div className="card text-center h-100 border-warning mt-5" style={{ borderRadius: "3rem", maxHeight:"44rem" }}>
                                     <div className="card-body">
                                         <h5 className="card-title mb-5 mt-2 fw-bolder">Google Ads</h5>
                                         <h6 className="card-subtitle text-muted">Plano Mensal</h6>
@@ -48,18 +48,17 @@ export default function Main() {
                                         <p className="card-text mb-3">Criação de Conversões com o Google Tag Manager </p>
                                         <p className="card-text mb-3"> Relatório Mensal</p>
                                     </div>
+                            
                                 </div>
-                                <div className="col-md-12 col-lg-2 text-center mt-3 mb-5 mt-5">
-                                    <Buttons color="btn-yellow" text="Saber Valores"/>
+                                <div className="text-center mt-3 mb-2">
+                                    <Buttons color="btn-yellow" text="Selecionar"/>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className="col-md-12 col-lg-2">
-                            <div className="mt-5">
-                                <div className="card card-paidmedia-gestaoredessociais">
+                            <div className="col-md-6 col-lg-3 mb-5 mt-2">
+                                <div className="card text-center h-100 border-warning mt-5" style={{ borderRadius: "3rem", maxHeight:"44rem" }}>
                                     <div className="card-body">
-                                        <h5 className="card-title mb-5 mt-2 fw-bolder">Facebook/Instagram & Google Ads</h5>
+                                        <h5 className="card-title mb-5 mt-2 fw-bolder">Facebook/Instagram &#38; Google Ads</h5>
                                         <h6 className="card-subtitle text-muted">Plano Mensal</h6>
                                         <p className="card-text mb-3 text-yellow fw-bold">PROFESSIONAL</p>
                                         <p className="card-text mb-3">Estratégia Digital</p>
@@ -68,16 +67,16 @@ export default function Main() {
                                         <p className="card-text mb-3">Criação de Conversões com o Google Tag Manager  </p>
                                         <p className="card-text mb-3">Relatório mensal</p>
                                     </div>
+                            
+                            
                                 </div>
-                                <div className="col-md-12 col-lg-2 text-center mt-3 mb-5 mt-5">
-                                    <Buttons color="btn-yellow" text="Saber Valores"/>
+                                <div className="text-center mt-3 mb-2">
+                                    <Buttons color="btn-yellow" text="Selecionar"/>
                                 </div>
                             </div>
-                        </div>
-
-                        <div className="col-md-12 col-lg-2">
-                            <div className="mt-5">
-                                <div className="card card-paidmedia-gestaoredessociais bg-yellow">
+                            
+                            <div className="col-md-6 col-lg-3 mb-5 mt-2">
+                                <div className="card text-center h-100 border-warning bg-yellow mt-5" style={{ borderRadius: "3rem", maxHeight:"44rem" }}>
                                     <div className="card-body">
                                         <h5 className="card-title mb-5 mt-2 fw-bolder"> Facebook/Instagram Google Ads</h5>
                                         <h6 className="card-subtitle text-muted">Plano Mensal/Anual</h6>
@@ -88,12 +87,13 @@ export default function Main() {
                                         <p className="card-text mb-3">Criação de Conversões com o Google Tag Manager  </p>
                                         <p className="card-text mb-3">Relatório mensal</p>
                                     </div>
+                        
                                 </div>
-                                <div className="col-md-12 col-lg-2 text-center mb-5 mt-5">
-                                    <Buttons color="btn-yellow" text="Saber Valores"/>
+                                <div className="text-center mt-3 mb-2">
+                                    <Buttons color="btn-yellow" text="Selecionar"/>
                                 </div>
                             </div>
-                        </div>
+
                         </div>
                     </div>
                 </div>
