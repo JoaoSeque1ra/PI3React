0d572f823a5f0fcfe6e70fd484735b18cfa1c317
Raquelmeida <raquelalmeida-15@outlook.pt>
2022-07-14 11:34:26 +0100

    regex e termos de privacidade

--- src/Pages/Client/Contactos/main.js
+++ src/Pages/Client/Contactos/main.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 import Breadcrumbs from '../../../Components/Breadcrumb';
-import CheckBox from '../../../Components/CheckBox';
-import CardsCheckBox from '../../../Components/CardsCheckBox';
 import Buttons from '../../../Components/Buttons';
 
 import ImagemWebsiteLojaOnline from "../../../Assets/Images/websites.jpg";
+
 
 export default function Main() {
     return(
@@ -19,27 +18,42 @@ export default function Main() {
                             <div className="col-md-12 col-lg-10 offset-lg-1">
                                 <div className="mt-5">
                                     <div className="input-group input-group-lg border cursor-pointer ">
-                                        <input type="text" class="form-control cursor-pointer border-0 rounded-0" placeholder="Nome"/>
+                                        <input type="text" class="form-control cursor-pointer border-0 rounded-0" pattern="^[a-z0-9_-]{3,15}$" placeholder="Nome"/>
                                     </div>
                                 </div>
 
                                 <div className="mt-5">
                                     <div className="input-group input-group-lg border cursor-pointer ">
-                                        <input type="text" className="form-control cursor-pointer border-0 rounded-0" placeholder="Email"/>
+                                        <input type="email" className="form-control cursor-pointer border-0 rounded-0" pattern="[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+" placeholder="Email"/>
                                     </div>
                                 </div>
 
                                 <div className="mt-5">
                                     <div className="input-group input-group-lg border cursor-pointer ">
-                                        <input type="text" className="form-control cursor-pointer border-0 rounded-0" placeholder="Telefone"/>
+                                        <input type="number" className="form-control cursor-pointer border-0 rounded-0" pattern="^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$" placeholder="Telefone"/>
                                     </div>
                                 </div>
 
-                                <div className="my-5">
+                                <div className="mt-5">
                                     <div className="input-group input-group-lg border cursor-pointer ">
-                                        <input type="text" className="form-control cursor-pointer border-0 rounded-0" placeholder="Empresa*"/>
+                                        <input type="text" className="form-control cursor-pointer border-0 rounded-0" pattern="^[a-z0-9_-]{3,15}$" placeholder="Empresa*"/>
                                     </div>
-                                </div>                             
+                                </div> 
+                                <div className="mt-5 form-check form-check-inline">
+                                    <input
+                                    class="form-check-input text-purple cursor-pointer"
+                                    type="checkbox"
+                                    id="inlineCheckbox1"
+                                    name="Aceito todos os termos e condições"
+                                    />
+                                    <label class="form-check-label" for="inlineCheckbox1">
+                                        Aceito todos os termos e condições
+                                    </label>
+                                </div>
+                                <div className='mt-1 ms-4 text-purple'>
+                                    <a href="#" className="text-purple"> Termos aqui.</a>
+
+                                </div>
                             </div>
 
                             <div className="col-md-5 offset-md-7 col-lg-6 offset-lg-5 text-end my-5">
