984d197ae7a62920d43a786602ff58f069fc3196
João Sequeira <joaosequeira28@hotmail.com>
2022-07-15 11:08:54 +0100

    Update index.js

--- src/Pages/Client/ComunicacaoConsultoria/index.js
+++ src/Pages/Client/ComunicacaoConsultoria/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Navbar from '../../../Components/NavbarClient';
 import Main from './main';
 
 export function ComunicacaoConsultoria() {
+    useEffect(()=>{
+        document.title ="Comunicação e Consultoria"
+    })
     return (  
         <>
             <Navbar />
