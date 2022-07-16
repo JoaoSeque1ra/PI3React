87b09439dcf7e439fffcb54d0098407a4a9d49a1
João Sequeira <joaosequeira28@hotmail.com>
2022-07-16 15:05:14 +0100

    new update

--- src/Components/Dashboard/PacksInput.js
+++ src/Components/Dashboard/PacksInput.js
@@ -5,7 +5,7 @@ export default function PacksInput(props) {
         <div className={"col-12 text-end " + props.className}>
             <label htmlFor="basic-url" className="form-label fw-light">{props.nomePack}</label>
             <div className="input-group mb-3">
-                <input type="text" className="form-control" id="basic-url" defaultValue={props.value} onChange={value => props.onChange(value)} pattern={props.pattern}/>
+                <input type={props.type} className="form-control" id="basic-url" defaultValue={props.value} onChange={value => props.onChange(value)} pattern={props.pattern}/>
             </div>
         </div>
     );
