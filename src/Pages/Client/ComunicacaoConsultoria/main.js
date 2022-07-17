618671a4d2467a78dfc6b2d7bbdfb83d9a6ce4a3
João Sequeira <joaosequeira28@hotmail.com>
2022-07-18 00:36:51 +0100

    Update main.js

--- src/Pages/Client/ComunicacaoConsultoria/main.js
+++ src/Pages/Client/ComunicacaoConsultoria/main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Breadcrumbs from '../../../Components/Breadcrumb';
 import CheckBox from '../../../Components/CheckBox';
@@ -12,6 +12,20 @@ export default function Main() {
     const [organizacaoEventos, setOrganizacaoEventos] = useState(false)
     const [acessoria, setAcessoria] = useState(false)
 
+    useEffect(() => {
+        const localSave = localStorage
+
+        for(let i = 0; i < localSave.length; i++) {
+            console.log(localSave.key(i))
+            if(localSave.key(i) === "89")
+                setMarketingComunicacao(true)
+            if(localSave.key(i) === "90")
+                setOrganizacaoEventos(true)
+            if(localSave.key(i) === "91")
+                setAcessoria(true)
+        }
+    },[])
+
     return(
         <main>
             <div className="container-fluid">
@@ -21,16 +35,25 @@ export default function Main() {
                             <Breadcrumbs route1="Orçamento" route2="/ Comunicação &#38; Consultoria" textColor="text-yellow"/>
 
                             <div className="col-md-12 col-lg-10 offset-lg-1">
-                                <CheckBox onClick={valeu=>setMarketingComunicacao(valeu.target.checked)} isActive={marketingComunicacao} name="Marketing e Comunicação" color="bg-checkBoxYellow" colorIcon="colorIconYellow" textColor="text-yellow" />
+                                <CheckBox onClick={valeu=>{
+                                    setMarketingComunicacao(valeu.target.checked)
+                                    saveLocal(valeu, "89")
+                                }} isActive={marketingComunicacao} name="Marketing e Comunicação" color="bg-checkBoxYellow" colorIcon="colorIconYellow" textColor="text-yellow" />
                                 <CardsCheckBox text="Consultoria é o acompanhamento ao nosso cliente em planos de comunicação publicidade e marketing.
                                 Desenhamos uma estratégia adaptada ao nosso cliente que são apresentadas e discutidas para objetivos
                                 positivos e lineares." />
 
-                                <CheckBox onClick={valeu=>setOrganizacaoEventos(valeu.target.checked)} isActive={organizacaoEventos} name="Organização de eventos" color="bg-checkBoxYellow" colorIcon="colorIconYellow" textColor="text-yellow" />
+                                <CheckBox onClick={valeu=>{
+                                    setOrganizacaoEventos(valeu.target.checked)
+                                    saveLocal(valeu, "90")
+                                }} isActive={organizacaoEventos} name="Organização de eventos" color="bg-checkBoxYellow" colorIcon="colorIconYellow" textColor="text-yellow" />
                                 <CardsCheckBox text="Planeamos e organizamos juntos um evento memorável para celebrar o aniversário da sua empresa ou
                                 datas importantes."/>
 
-                                <CheckBox onClick={valeu=>setAcessoria(valeu.target.checked)} isActive={acessoria} name="Assessoria e criação de conteúdo" color="bg-checkBoxYellow" colorIcon="colorIconYellow" textColor="text-yellow" />
+                                <CheckBox onClick={valeu=>{
+                                    setAcessoria(valeu.target.checked)
+                                    saveLocal(valeu, "91")
+                                }} isActive={acessoria} name="Assessoria e criação de conteúdo" color="bg-checkBoxYellow" colorIcon="colorIconYellow" textColor="text-yellow" />
                                 <CardsCheckBox text="Ligado ao marketing digital, assessoria de impressa procura criar uma boa relação com os canais de
                                 informação e assim dessa forma gerar tráfego para a empresa."/>
 
@@ -55,5 +78,10 @@ export default function Main() {
         </main>
     );
 
+    function saveLocal(value, nome) {
+        if(value.target.checked)
+            return localStorage.setItem(nome,value.target.checked)
+        localStorage.removeItem(nome)
+    }
     
 }
